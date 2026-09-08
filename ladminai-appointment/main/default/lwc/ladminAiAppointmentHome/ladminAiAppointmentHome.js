2777b6f242c53e2c0695929ad2d049aa9f3e6b2c
nexussync11 <testdevds20@gmail.com>
2026-09-08 18:28:31 +0530

    feat: add LadminAI Smart Appointments logo

--- ladminai-appointment/main/default/lwc/ladminAiAppointmentHome/ladminAiAppointmentHome.js
+++ ladminai-appointment/main/default/lwc/ladminAiAppointmentHome/ladminAiAppointmentHome.js
@@ -1,3 +1,6 @@
 import { LightningElement } from 'lwc';
+import productLogo from '@salesforce/resourceUrl/LadminAI_Smart_Appointments_Logo';
 
-export default class LadminAiAppointmentHome extends LightningElement {}
+export default class LadminAiAppointmentHome extends LightningElement {
+    productLogo = productLogo;
+}
